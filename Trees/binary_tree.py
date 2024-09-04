82bb601c04271a0cd2aeff4ef0fce59a13970fb7
Dev-Rikshith <46825752+Dev-Rikshith@users.noreply.github.com>
2024-09-04 23:42:27 +0000

    Added Tree Sum function

--- Trees/binary_tree.py
+++ Trees/binary_tree.py
@@ -49,20 +49,40 @@ class Node():
                 queue.append(current.right)
 
     def tree_includes_recursive(self, target):
-        if(self.val == target):
-            return True
-        if(self == None):
+        if self is None:
             return False
-        return self.tree_includes_recursive(self.right) or self.tree_includes_recursive(self.left)
+        
+        if self.val == target:
+            return True
+        
+        return (self.left.tree_includes_recursive(target) if self.left else False) or (self.right.tree_includes_recursive(target) if self.right else False)
 
+    def tree_sum(self):
+        if self is None:
+            return 0
+        
+        return self.val + (self.left.tree_sum() if self.left else 0) + (self.right.tree_sum() if self.right else 0)
 
 if __name__ == "__main__":
-    a = Node("a")
-    b = Node("b")
-    c = Node("c")
-    d = Node("d")
-    e = Node("e")
-    f = Node("f")
+    # a = Node("a")
+    # b = Node("b")
+    # c = Node("c")
+    # d = Node("d")
+    # e = Node("e")
+    # f = Node("f")
+
+    # a.left = b
+    # a.right = c
+    # b.left = d
+    # b.right = e
+    # c.right = f
+
+    a = Node(3)
+    b = Node(11)
+    c = Node(4)
+    d = Node(4)
+    e = Node(2)
+    f = Node(1)
 
     a.left = b
     a.right = c
@@ -70,8 +90,9 @@ if __name__ == "__main__":
     b.right = e
     c.right = f
 
-    if a.tree_includes_recursive("e"):
-        print("Element found")
+
+
+    print(a.tree_sum())
 
     #     a   
     #    /  \
